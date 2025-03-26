03da58e99a716247a4ece9c15d269b4ea8851e45
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2025-03-26 14:05:31 +0100

    feat: link to public vercel page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-4xl font-bold">iframe resizer parent</h1>
-        <iframe ref={iframe} src="http://localhost:3000" className="max-w-screen-lg w-full mx-auto min-h-64" />
+        <iframe ref={iframe} src="https://iframe-resizer-child.vercel.app" className="max-w-screen-lg w-full mx-auto min-h-64" />
       </main>
     </div>
   );
